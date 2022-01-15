6879bf9d5bef38a5307affc74b252f13b173e83d
student <abdullahnazari2001@gmail.com>
2022-01-16 04:39:31 +0600

    responsiveness added

--- public/script.js
+++ public/script.js
@@ -2,10 +2,28 @@ const toggle = document.querySelector("[data-toggle]");
 const nav = document.querySelector("[data-nav]");
 const navbar = document.querySelector("[data-navbar]");
 const logo = document.querySelector("[data-logo]");
+const control = document.querySelector(".control");
+const close = document.querySelector(".close");
 toggle.addEventListener("click", handleHover);
+close.addEventListener("click", handleClose);
+window.addEventListener("resize", checkResize);
 
-function handleHover(e) {
-  console.log(e.target.classList.contains("fa-hamburger"));
+function handleHover() {
+  nav.style.display = "block";
+  toggle.style.display = "none";
+  logo.style.display = "none";
+}
 
-  e.target.className.replace("fas fa-align-justify", `fas fa-hamburger`);
+function handleClose() {
+  nav.style.display = "none";
+  toggle.style.display = "block";
+  logo.style.display = "block";
+}
+
+function checkResize() {
+  if (window.innerWidth > 768) {
+    nav.style.display = "flex";
+  } else {
+    nav.style.display = "none";
+  }
 }
